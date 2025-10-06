32ece7f5f82ddbc272081fb280e1776a86838875
Magyar Levente <magyar.levente25@gmail.com>
2025-10-06 18:42:06 +0200

    hetedik sziv

--- 7.py
+++ 7.py
@@ -3,16 +3,16 @@ from sense_hat import SenseHat
 sense = SenseHat()
 
 w = (255, 255, 255)
-b = (0, 0, 255)
+b = (255, 0, 0)
 
 smiley_pixels = [
                  w, w, w, w, w, w, w, w,
                  w, b, b, w, w, b, b, w,
-                 w, b, b, w, w, b, b, w,
-                 w, w, w, w, w, w, w, w,
-                 w, w, w, w, w, w, w, w,
-                 w, b, w, w, w, w, b, w,
-                 w, w, b, b, b, b, w, w,
+                 r, r, r, r, r, r, r, r,
+                 r, r, r, r, r, r, r, r,
+                 w, r, r, r, r, r, r, w,
+                 w, w, r, r, r, r, w, w,
+                 w, w, w, r, r, w, w, w,
                  w, w, w, w, w, w, w, w]
 
 sense.set_pixels(smiley_pixels)
