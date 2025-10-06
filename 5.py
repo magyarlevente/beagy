86e78efa07105083a3a981437a2d541f1bad7c95
Magyar Levente <magyar.levente25@gmail.com>
2025-10-06 18:28:27 +0200

    ötödik feladat

---
+++ 5.py
@@ -0,0 +1,18 @@
+from sense_hat import SenseHat
+import random
+
+sense = SenseHat() 
+
+def random_colour():
+    random_red = randint(0, 255)  
+    random_green = randint(0, 255) 
+    random_blue = randint(0, 255) 
+    return (random_red, random_green, random_blue)
+
+sense.show_letter("L", random_colour())
+sleep(1)
+sense.show_letter("e", random_colour())
+sleep(1)
+sense.show_letter("v", random_colour())
+sleep(1)
+sense.show_letter("i", random_colour())
